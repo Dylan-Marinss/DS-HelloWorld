f0c35247e75db0e0d96ae756e3b1be299b7731b6
Dylan-Marinss <dylanmarins24@gmail.com>
2025-07-29 22:14:14 -0300

    Aula 01

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+namespace HelloWord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           //Console.WriteLine("Hello World!");
+
+           Console.WriteLine("Digite seu nome:");
+           string nome = Console.ReadLine();
+
+           Console.WriteLine($"Seu nome tem {nome.Length} caracteres");
+
+           Console.WriteLine($"Digite a data do seu nascimento:");
+           DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+           int qtdDiasVividos = DateTime.Now.Subtract(dtNascimento).Days;
+           Console.WriteLine("Os dias vividos até hoje são: " + qtdDiasVividos); 
+
+           Console.ReadKey();
+        }
+    }
+}
